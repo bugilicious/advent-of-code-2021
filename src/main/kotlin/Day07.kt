554214549d6398494a128601c07ab2bbe74eeebf
Daniel Obermüller <daniel.obermueller@sonem.org>
2021-12-07 19:38:43 +0100

    adds day07

---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,17 @@
+import kotlin.math.abs
+
+fun main() {
+    val crabPositions = readInput("Day07_test")[0].split(',').map { it.toInt() }
+
+    fun calculateMinimumFuelConsumption(fuelConsumptionCalculation: (crabPosition: Int, targetPosition: Int) -> Int) =
+        (0..crabPositions.maxOf { it })
+            .map { targetPosition -> crabPositions.map { fuelConsumptionCalculation(it, targetPosition) }.sum() }
+            .minByOrNull { it }
+            ?: 0
+
+    println(calculateMinimumFuelConsumption(::part1))
+    println(calculateMinimumFuelConsumption(::part2))
+}
+
+private fun part1(crabPosition: Int, targetPosition: Int) = abs(crabPosition - targetPosition)
+private fun part2(crabPosition: Int, targetPosition: Int) = (1..abs(crabPosition - targetPosition)).sum()
