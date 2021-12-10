071cb31423172702eb7724265538b76759254ddb
Daniel Obermüller <daniel.obermueller@sonem.org>
2021-12-10 12:29:31 +0100

    adds PuzzleSolver.kt

---
+++ src/main/kotlin/PuzzleSolver.kt
@@ -0,0 +1,4 @@
+interface PuzzleSolver {
+    fun solvePart1(): Number
+    fun solvePart2(): Number
+}
--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,24 +1,30 @@
 import kotlin.math.abs
-import kotlin.system.measureTimeMillis
 
 fun main() {
-    val crabPositions = readInput("day07/Day07")[0].split(',').map { it.toInt() }
-
-    measureTimeMillis {
-        println(crabPositions.calculateMinimumFuelConsumption(::part1))
-    }.also { println("execution time: $it ms") }
-    measureTimeMillis {
-        println(crabPositions.calculateMinimumFuelConsumption(::part2))
-    }.also { println("execution time: $it ms") }
+    with(Day07) {
+        println(solvePart1())
+        println(solvePart2())
+    }
 }
 
-inline fun List<Int>.calculateMinimumFuelConsumption(
-    fuelConsumptionCalculation: (crabPosition: Int, targetPosition: Int) -> Int
-) =
-    (0..maxOf { it })
-        .map { targetPosition -> map { fuelConsumptionCalculation(it, targetPosition) }.sum() }
-        .minByOrNull { it }
-        ?: 0
+object Day07 : PuzzleSolver {
 
-private fun part1(crabPosition: Int, targetPosition: Int) = abs(crabPosition - targetPosition)
-private fun part2(crabPosition: Int, targetPosition: Int) = (1..abs(crabPosition - targetPosition)).sum()
+    private val crabPositions = readInput("day07/Day07")[0].split(',').map { it.toInt() }
+
+    override fun solvePart1() = crabPositions.calculateMinimumFuelConsumption(::simpleFuelConsumptionCalculation)
+    override fun solvePart2() = crabPositions.calculateMinimumFuelConsumption(::advancedFuelConsumptionCalculation)
+
+    private fun simpleFuelConsumptionCalculation(crabPosition: Int, targetPosition: Int) =
+        abs(crabPosition - targetPosition)
+
+    private fun advancedFuelConsumptionCalculation(crabPosition: Int, targetPosition: Int) =
+        (1..abs(crabPosition - targetPosition)).sum()
+
+    private inline fun List<Int>.calculateMinimumFuelConsumption(
+        fuelConsumptionCalculation: (crabPosition: Int, targetPosition: Int) -> Int
+    ) =
+        (0..maxOf { it })
+            .map { targetPosition -> map { fuelConsumptionCalculation(it, targetPosition) }.sum() }
+            .minByOrNull { it }
+            ?: 0
+}
